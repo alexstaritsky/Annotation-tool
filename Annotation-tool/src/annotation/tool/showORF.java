2fe8a22bd9b01c44046518cb7cab8f0fce18f7b7
Angga Sondang <Angga Sondang@DESKTOP-54LIM82>
2017-04-03 14:31:35 +0200

    vind start and stop codons

--- Annotation-tool/src/annotation/tool/showORF.java
+++ Annotation-tool/src/annotation/tool/showORF.java
@@ -5,10 +5,7 @@
  */
 package annotation.tool;
 
-//import static annotation.tool.showProtein.showProtein;
-
-import java.util.Arrays;
-
+import java.util.*;
 
 /**
  *
@@ -16,21 +13,33 @@ import java.util.Arrays;
  */
 public class showORF {
     public static void main(String[]args){
+        //Test sequentie
         String sequence = "CCACACCACACCCACACACCCAUGCACACCACACCACACACCACACCACACCCACACACACACATCCTAACACTACCCTAACACAGCCCTTAA";
+
+        //Arraylist voor ORFs
+        List orfs = new ArrayList();
         
-        //String AminoAcid = showProtein.translate(sequence);
-        //System.out.println(AminoAcid);
+        //Variabelen
         String codon;
         int currentStart=0;
         String START = "M";
+        String STOP = "*";
+        boolean inORF = false;
         
+        //Loop door het DNA sequentie
         for(int i=0; i< (sequence.length())/3;i++){
+            
+            //Leest per 3 nucleotiden
             codon = sequence.substring(i*3,i*3+3);
             
             String aa = showProtein.translate(codon);
-            System.out.println(codon+"-"+aa);
-                
-        }
-        
+                if (aa.equals(START) && inORF==false){
+                    inORF = true;
+                }
+                if (aa.equals(STOP) && inORF==true){
+                    inORF = false;
+                }
+                System.out.println(codon+" "+aa+" "+inORF);
+            }
     }
 }
